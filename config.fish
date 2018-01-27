3da7c37b4632513c56b57cdc03fec29bf8299e37
Wheeler9 <cz.matyas@gmail.com>
2018-01-27 13:22:37 +0100

    Tmux statusbar updated

---
+++ config.fish
@@ -0,0 +1,49 @@
+
+
+set_color cyan
+echo 'fish.config loaded'
+set -Ux EDITOR nvim
+set -Ux VISUAL EDITOR nvim
+set PATH /home/matyi/miniconda3/bin $PATH
+source (conda info --root)/etc/fish/conf.d/conda.fish
+
+
+
+alias p "python"
+alias df "df -hT"
+alias bp "bpython"
+alias coin "python .coin.py"
+alias vim "nvim"
+alias sd 'ls -lh'
+alias ip 'jupyter notebook'
+alias rr 'ranger'
+alias cc 'calcurse'
+alias pdf 'xdg-open'
+alias ts 's-tui'
+alias vim 'nvim'
+alias ll 'clear'
+alias xx 'exit'
+alias p 'python'
+alias n 'nautilus .'
+alias t 'tmux'
+alias bp 'bpython'
+alias OTP 'cd /media/matyi/Data/PYTHON/PYTHON3/bokehserver/; bokeh serve --show finance.py'
+alias colors '~/.colors.sh'
+alias wthr 'curl wttr.in/budapest'
+alias enbeep 'sudo modprobe pcspkr'
+alias coin 'p ~/.coin.py'
+alias trainy 'p ~/.trainy.py'
+alias beepy 'sudo modprobe pcspkr; p ~/.beepy.py'
+
+alias zvim '/usr/bin/zsh; nvim'
+
+alias cfb 'nvim ~/.bashrc'
+alias cfa 'nvim ~/.bash_aliases'
+alias cfr 'nvim ~/.config/ranger/rc.conf'
+alias cff 'nvim ~/.config/fish/config.fish'
+alias cft 'nvim ~/.tmux.conf'
+
+function fish_greeting
+
+    fish_logo
+end
